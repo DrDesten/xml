00c5839f979df69fc7efade01d2e134e2bc4fc7c
DrDesten <>
2024-06-20 19:10:04 +0200

    can parse xml now

---
+++ bin/index.js
@@ -0,0 +1,80 @@
+import { parseXML } from "./parser.js"
+
+const empty = ``
+const text = `Sample "Text"!`
+const node = `<root></root>`
+const nodetext = `<root>Sample "Text"!</root>`
+
+const selfclose = `<root/>`
+const attr = `<root attr="test"/>`
+const boolattr = `<root attr/>`
+
+console.log( parseXML( empty ) )
+console.log( parseXML( text ) )
+console.log( parseXML( node ) )
+console.log( parseXML( nodetext ) )
+
+console.log( parseXML( selfclose ) )
+console.log( parseXML( attr ) )
+console.log( parseXML( boolattr ) )
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>DrDesten - Home</title>
+    <meta name="description" content="Homepage of DrDesten, creator of DrDestens MinecraftShaders and Stracciatella Shaders. Check out my tools: Gaussian Kernel Calculator, Vogel Disk Calculator, Array Progressifier, Probability distribution analyzer"/>
+    <link rel="icon" type="image/png" href="icons/icon_main.png"/>
+<link rel="stylesheet" href="defaultstyles.css"/>
+<link rel="stylesheet" href="general.css"/>
+    <noscript>
+        <link rel="stylesheet" href="noscript.css"/>
+    </noscript>
+</head>
+
+<body>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script src="svg.js"></script>
+    <script src="general.js"></script>
+
+    <nav>
+        <table class="nav" style="background-color: transparent;">
+            <tr class="nav">
+                <th class="nav">
+                    <a class="nav" href="3d/">😍 3D</a>
+                </th>
+                <th class="nav">
+                    <a class="nav" href="projects/">Projects</a>
+                </th>
+                <th class="nav">
+                    <a class="nav" href="tools/">Tools</a>
+                </th>
+            </tr>
+        </table>
+    </nav>
+
+    <div id="nav-padding"></div>
+
+    <header class="fillscreen">
+        <div class="fillscreen-center">
+            <h1 class="typewriter" float-depth="1.0" float-smooth>Hello, I'm DrDesten</h1>
+            <h2 id="header-subtitle" class="header-subtitle" float-depth="0.5" float-smooth>check out my work</h2>
+        </div>
+    </header>
+
+    <a href="https://www.github.com/DrDesten/" target="_blank" rel="noopener noreferrer"><img class="icon" src="icons/GitHub Mark.svg" alt="GitHub Mark" width="100" height="100"/></a>
+
+    <div id="canvas" style="position: absolute;top:0;bottom:0;right:0;left:0;z-index:-1;"></div>
+
+    <script src="style.js"></script>
+    <script src="animation.js"></script>
+    <script src="svgbackground.js"></script>
+
+</body>
+
+</html>`
+
+console.log( parseXML( html ) )
--- bin/parser.js
+++ bin/parser.js
@@ -1,3 +1,11 @@
+/**
+ * @typedef {Object} XMLNode
+ * @property {string} name
+ * @property {{[attribute: string]: string}} attributes
+ * @property {(XMLNode|string)[]} children
+ * @property {XMLNode} parent
+ */
+
 class Lexer {
     /** @param {string} text */
     constructor( text ) {
@@ -8,19 +16,128 @@ class Lexer {
     /** @param {RegExp} [comparison] */
     peek( comparison ) {
         const char = this.text[this.index]
-        const valid = ( !comparison || comparison.test(char) )
+        const valid = ( !comparison || comparison.test( char ) )
         return valid ? char : ""
     }
     /** @param {RegExp} [comparison] */
     advance( comparison ) {
         const char = this.text[this.index]
-        const valid = ( !comparison || comparison.test(char) )
-        if ( !valid ) throw new Error(`advance(): Expected ${comparison}, got '${char}'`)
+        const valid = ( !comparison || comparison.test( char ) )
+        if ( !valid ) throw new Error( `advance(): Expected ${comparison}, got '${char}'` )
+        else this.index++
         return char
     }
 
-    /** @param {RegExp} [comparison] */
+    /** @param {RegExp} comparison */
+    peekWhile( comparison ) {
+        let offset = 0
+        let result = ""
+        while ( comparison.test( this.text[this.index + offset] ) )
+            result += this.text[this.index + offset++]
+        return result
+    }
 
+    /** @param {RegExp} comparison */
+    advanceWhile( comparison ) {
+        let result = ""
+        while ( comparison.test( this.text[this.index] ) )
+            result += this.text[this.index++]
+        return result
+    }
+
+    /** @param {RegExp} comparison */
+    peekAll( comparison ) {
+        const match = this.text.slice( this.index ).match( comparison )
+        if ( !match || match.index !== 0 ) return ""
+        return match[0]
+    }
+
+    /** @param {RegExp} comparison */
+    advanceAll( comparison ) {
+        const match = this.text.slice( this.index ).match( comparison )
+        if ( !match || match.index !== 0 )
+            throw new Error( `advanceAll(): Expected ${comparison}, got '${this.text.slice( this.index, this.index + 10 )}'` )
+        this.index += match[0].length
+        return match[0]
+    }
+
+
+    // Utils
+
+    advanceIdentifier() {
+        return this.advanceAll( /[a-zA-Z0-9_-]+/ )
+    }
+}
+
+export function parseXML( text ) {
+    text = text.replace( /<!--[^]*?-->/g, "" )
+    const lexer = new Lexer( text )
+
+    /** @param {XMLNode} parent  */
+    function parseNode( parent ) {
+        // Parse Begin Tag
+        lexer.advance( /</ )
+        const name = lexer.advanceIdentifier()
+        lexer.advanceWhile( /\s/ )
+
+        // Parse Attributes
+        const attributes = {}
+        while ( !lexer.peekAll( /\/?>/ ) ) {
+            const key = lexer.advanceIdentifier()
+            lexer.advanceWhile( /\s/ )
+
+            if ( lexer.peek( /=/ ) ) {
+                lexer.advance( /=/ )
+                lexer.advanceWhile( /\s/ )
+                const value = lexer.advanceAll( /(["'])[^]*?\1/ ).slice( 1, -1 )
+                attributes[key] = value
+            } else {
+                attributes[key] = ""
+            }
+
+            lexer.advanceWhile( /\s/ )
+        }
+
+        const node = { name, attributes, children: [], parent }
+
+        if ( lexer.peekAll( /\/>/ ) ) {
+            lexer.advanceAll( /\/>/ )
+            return node
+        }
+
+        // Parse Children
+        lexer.advance( />/ )
+        node.children = parseChildren( node )
+        lexer.advanceAll( RegExp( `</${name}>` ) )
+
+        return node
+    }
+
+    /** @param {XMLNode} parent  */
+    function parseChild( parent ) {
+        if ( lexer.peekAll( /<[a-zA-Z]/ ) )
+            return parseNode( parent )
+        else
+            return lexer.advanceAll( /([^<]|<[^a-zA-Z\/])+/ )
+    }
+
+    /** @param {XMLNode} parent  */
+    function parseChildren( parent ) {
+        const children = []
+        const end = RegExp( `</${parent.name}>` )
+        while ( lexer.peek() && !lexer.peekAll( end ) )
+            children.push( parseChild( parent ) )
+        return children
+    }
+
+    function parseRoot() {
+        const children = []
+        while ( lexer.peek() )
+            children.push( parseChild( null ) )
+        return children
+    }
+
+    return parseRoot()
 }
 
 /** @param {string} text */
